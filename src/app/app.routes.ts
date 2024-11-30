2e02709821f5fd269086e7a8fb57ecfe433b3acf
mspirovv <mspirov@abv.bg>
2024-11-30 18:25:08 +0200

    Added guards
    Auth & Guest

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { GuestGuard } from './guards/guest.guard';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/catalog',pathMatch: 'full'},
     {path: 'catalog', component: CatalogComponent},
     {path: 'catalog/:id', component: ProductDetailsComponent},
-    {path: 'login', component: LoginComponent},
+    {path: 'login', component: LoginComponent, canActivate: [GuestGuard]},
     {path: 'register', component: RegisterComponent},
     {path: 'profile', component: ProfileComponent},
 
